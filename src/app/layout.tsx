bbc45c90e03ebbf22f5fa31e3637de3a1da6d69f
ridwandxp12 <ridwan.dxp@gmail.com>
2026-01-22 20:28:50 +0700

    add : layout for main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import '@/i18n'
+import LayoutMain from "@/app/components/LayoutMain";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <LayoutMain >
+          {children}
+        </LayoutMain>
       </body>
     </html>
   );
